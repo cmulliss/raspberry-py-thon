761c9ee53223f3a602b460bc55927b332c009f3a
Cherry Mulliss <cmulliss@gmail.com>
2016-05-19 20:08:57 +0100

    Added to dice programs

---
+++ twoDiceWithWhile.py
@@ -0,0 +1,24 @@
+# 2 Dice
+
+from random import randint
+
+throwOne = randint(1, 6)
+throwTwo = randint(1, 6)
+
+total = throwOne + throwTwo
+        
+noOfThrows = int(input('You have 2 dice, how many throws would you like? '))
+throws = noOfThrows + 1
+for x in range(1, throws):
+ 
+ 
+    print(total)
+
+    while not (throwOne == 6 and throwTow == 6):
+ 
+        print('total')
+        print('Double 6 thrown')
+       
+    
+        
+
